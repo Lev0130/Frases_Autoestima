3deeb334391b017e2279236d31f36f6cfbd406a2
Lev_Shakhov <leoshakhovcontacto@gmail.com>
2026-01-17 18:14:40 +0100

    Versión 2.0, introducido el argumento --start

--- diario.py
+++ diario.py
@@ -2,10 +2,9 @@
 #visualizar e interactuar con agparse
 #crear un loggin
 import argparse
-import tkinter as tk
 from datetime import date
-from tkinter import messagebox
-respuestas_para_guardar = dict()
+
+respuestas_para_guardar = []
 
 frases_guardadas= list()
 
@@ -17,22 +16,35 @@ frases = [
 
 parser = argparse.ArgumentParser(description="Rellenar frases Autoestima")
 
-parser.add_argument("-1", "--frase_1", type=str, required=True)
-parser.add_argument("-2", "--frase_2", type=str, required=True)
-parser.add_argument("-3", "--frase_3", type=str, required=True)
+
+parser.add_argument("-f1", "--frase_1", type=str, required=False)
+parser.add_argument("-f2", "--frase_2", type=str, required=False)
+parser.add_argument("-f3", "--frase_3", type=str, required=False)
+parser.add_argument("--start", action="store_true", required=False)
 
 args = parser.parse_args()
+
+if args.start:
+    print(
+    "\n"
+    "Estas son las frases que tienes que rellenar:\n\n"
+    "-f1 Vivir de manera consciente significa para mí,\n"
+    "-f2 Si aporto un 5% más de consciencia a mis actividades de hoy,\n"
+    "-f3 Si presto más atención hoy a mi manera de relacionarme con las personas,\n"
+)
+
 respuestas = [args.frase_1, args.frase_2, args.frase_3]
 
-for frase in frases:
-    x=0
-    respuestas_para_guardar[frase] = respuestas[x]
-    x= x+1
+if all(respuestas): 
 
-print(respuestas_para_guardar)
+    respuestas_para_guardar = list(zip(frases,respuestas))
 
-with open("autoestima.txt", "a", encoding="utf-8") as file:
-                file.write(f"{date.today()}\n")
-                for frase, respuesta in respuestas_para_guardar:
-                    file.write(f"{frase}{respuesta}.\n\n")
-                file.write("\n\n")
+    print(respuestas_para_guardar)
+
+    with open("autoestima.txt", "a", encoding="utf-8") as file:
+                    file.write(f"{date.today()}\n")
+                    for frase, respuesta in respuestas_para_guardar:
+                        file.write(f"{frase}{respuesta}.\n\n")
+                    file.write("\n\n")
+
+                
